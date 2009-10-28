1a179a154eb649899ed122cda2a40fb9bdb44161
Adam Davies <adzdavies@gmail.com>
2009-10-28 14:29:18 +1030

    Collapsed all reports into one...

--- app/controllers/management_reports_controller.rb
+++ app/controllers/management_reports_controller.rb
@@ -1,6 +1,17 @@
 class ManagementReportsController < ApplicationController
   unloadable
-  
+
+
+  #
+  # All issues created in period (new)
+  # All issues with time-entries for period
+  # All remaining issues (not completed)
+  #
+  # Merge the lot
+  #
+  # Include time-spent in total
+  # Include time-spent in period
+  #
   def index
     @project = Project.find(params[:project_id])
 
@@ -13,29 +24,25 @@ class ManagementReportsController < ApplicationController
       @project.versions.reject(&:completed?)
     end
 
-    new_issues = @project.issues.find(:all,
-      :conditions => {
-        :created_on => @from..(@to+1) # cheat -- +1 as time is from midnight
-      }
-    )
-    @new_issues_report = new_issues_report(new_issues)
+    # All issues created in period (new)
+    # All issues with time-entries for period
+    # All remaining issues (not completed)
+    #
+    # Merge the lot
+    @reportable_issues = get_reportable_issues(@project, @from, @to)
 
+    data = Munger::Data.load_data(@reportable_issues.map{|issue|issue_report(issue)})
+    data.transform_column(:estimated_hours) do |row|
+      row.estimated_hours || 0
+    end
 
-    @time_entries = TimeEntry.all(:conditions => {
-        :spent_on => (@from..@to)
-      }
-    )
-    @time_entries_report = time_entries_report(@time_entries)
+    @reportable_issues_report = Munger::Report.from_data(data).process
+    @reportable_issues_report.columns [:id, :status, :project, :tracker, :priority, :subject, :estimated_hours, :total_time_spent, :time_spent]
+    @reportable_issues_report.aggregate(:sum => :estimated_hours).process
+    humanize_column_titles(@reportable_issues_report)
 
+    @reportable_issues_report.style_rows('issue') {|row| true} # always styles rows as issues
 
-
-    @remaining_issues = @project.issues.find(:all,
-      :conditions => {
-        :status_id => IssueStatus.all(:conditions => {:is_closed => false}),
-        :fixed_version_id => @version
-      }
-    )
-    @remaining_issues_report = remaining_issues_report(@remaining_issues)
 
     @active_versions = @project.versions.reject(&:completed?)
 
@@ -43,15 +50,9 @@ class ManagementReportsController < ApplicationController
       format.csv do
         csv_text = "*Quicktravel Management Reports*
 
-New Issues
-#{Munger::Render.to_csv(@new_issues_report)}
-
-Time Entries
-#{Munger::Render.to_csv(@time_entries_report)}
-
-Remaining Issue
-#{Munger::Render.to_csv(@remaining_issues_report)}
-        "
+All Work: Done & Remaining
+#{Munger::Render.to_csv(@reportable_issues_report)}
+"
         render :text => csv_text, :mime_type => "text/csv"
       end
       format.html {}
@@ -59,96 +60,37 @@ Remaining Issue
   end
 
   private
-  def new_issues_report(new_issues)
-    data = Munger::Data.load_data(new_issues.map{|issue|
-        issue.attributes.merge(
-          :status   => issue.status.to_s,
-          :project  => issue.project.to_s,
-          :tracker  => issue.tracker.to_s,
-          :priority => issue.priority.to_s
-        )
+  def get_reportable_issues
+    new_issues = @project.issues.all(:conditions => {
+        :created_on => @from..(@to+1) # cheat -- +1 as time is from midnight
+    })
+
+    time_entries = TimeEntry.all(:conditions => {
+        :spent_on => (@from..@to)
       }
     )
-    data.transform_column(:estimated_hours) do |row|
-      row.estimated_hours || 0
-    end
+    issues_worked_on = time_entries.map(&:issue).compact.uniq
 
-    new_issues_report = Munger::Report.from_data(data).process
-    new_issues_report.columns [:id, :status, :project, :tracker, :priority, :subject, :estimated_hours]
-    new_issues_report.aggregate(:sum => :estimated_hours).process
-    humanize_column_titles(new_issues_report)
 
-    new_issues_report.style_rows('issue') {|row| true} # always styles rows as issues
-
-    new_issues_report
+    remaining_issues = @project.issues.find(:all,
+      :conditions => {
+        :status_id => IssueStatus.all(:conditions => {:is_closed => false}),
+        :fixed_version_id => @version
+      }
+    )
+    new_issues + issues_worked_on + remaining_issues
   end
 
 
-  def time_entries_report(time_entries)
-    # Get data
-    time_entry_data = Munger::Data.load_data(time_entries.map{|time_entry|
-        {
-          :project  => "#{time_entry.project}",
-          :activity => "#{time_entry.activity}",
-          :created  => time_entry.created_on.to_date,
-          :hours    => time_entry.hours,
-          :issue    => "#{time_entry.issue}",
-          :comments => time_entry.comments
-        }
-      }
+  def issue_report(issue, from, to)
+    issue.attributes.merge(
+      :status   => issue.status.to_s,
+      :project  => issue.project.to_s,
+      :tracker  => issue.tracker.to_s,
+      :priority => issue.priority.to_s,
+      :time_spent => issue.time_entries.select{|t| (from..to).include? t.spent_on}.sum(:hours),
+      :total_time_spent => issue.time_entries.sum(:hours)
     )
-
-    # Build report
-    time_entry_report = Munger::Report.from_data(time_entry_data)
-    time_entry_report.columns [:project, :activity, :created, :hours, :issue, :comments]
-
-    humanize_column_titles(time_entry_report)
-
-    time_entry_report.process
-
-    time_entry_report.style_rows('issue'){|row|true}
-    time_entry_report.aggregate(:sum => :hours).process
-
-    time_entry_report.sort([:project, :activity])
-    time_entry_report.subgroup([:project, :activity], :with_headers => true)
-
-    # Return it
-    time_entry_report.process
-  end
-
-
-  def remaining_issues_report(remaining_issues)
-    data = Munger::Data.load_data(remaining_issues.map{|issue|
-        issue.attributes.merge(
-          :status   => issue.status.to_s,
-          :project  => issue.project.to_s,
-          :tracker  => issue.tracker.to_s,
-          :priority => issue.priority.to_s
-        )
-      }
-    )
-
-    data.transform_column(:estimated_hours) do |row|
-      row.estimated_hours || 0
-    end
-
-    data.add_column(:time_spent) do |row|
-      puts row.inspect
-      TimeEntry.all(:conditions => {:issue_id => row.id}).map(&:hours).sum
-    end
-
-    data.transform_column(:done_ratio) do |row|
-      "#{row.done_ratio}%"
-    end
-
-    new_issues_report = Munger::Report.from_data(data).process
-    new_issues_report.columns [:id, :tracker, :priority, :subject, :estimated_hours, :done_ratio, :time_spent]
-    new_issues_report.aggregate(:sum => [:estimated_hours, :time_spent]).process
-    humanize_column_titles(new_issues_report)
-
-    new_issues_report.style_rows('issue') {|row| true} # always styles rows as issues
-
-    new_issues_report
   end
 
 
